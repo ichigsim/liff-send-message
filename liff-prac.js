f0a0bcb421dc56d36b0f0a55a3b1a93d44f75cc2
ichigism <ichigo.735@gmail.com>
2018-06-28 00:00:49 +0900

    #11 fix js

--- liff-prac.js
+++ liff-prac.js
@@ -6,45 +6,17 @@ window.onload = function (e) {
 
 function initializeApp(data) {
 
-    function onButtonClick() {
-        // output = document.getElementById("output");
-        // textbox = document.getElementById("input");
-        // output.innerText = textbox.value;
-        liff.sendMessages({
-            type: 'text',
-            text: textbox
-        }).then((result) => {
-            window.alert("メッセージを送信しました！");
-        }).catch((err) => {
-            window.alert("エラーは次のようになります: " + err);
-        });
-    }
-
-
-      document.getElementById('sendmessage').addEventListener('click', function () {
-        liff.sendMessages({
-            type: 'text',
-            text:  "test"
-        }).then(function () {
-            window.alert("メッセージを送信しました！");
-        }).catch(function (error) {
-            window.alert("エラーは次のようになります: " + error);
-        });
-    });
-
-    
-    // sendMessages call
     document.getElementById('sendmessagebutton').addEventListener('click', function () {
         output = document.getElementById("output");
         textbox = document.getElementById("input");
         output.innerText = textbox.value;
         liff.sendMessages([{
             type: 'text',
-            text: "" + output.value + ""
+            text: "taro" + output.value + "tanaka"
         }]).then(function () {
             liff.closeWindow()
         }).catch(function (error) {
-            window.alert("Error: " + error);
+            window.alert("エラーは次のようになります: " + error);
         });
     });
 
