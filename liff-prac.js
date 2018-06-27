a73728d23f5b551e75fa648ae3463abb7ff63193
ichigism <ichigo.735@gmail.com>
2018-06-28 00:35:03 +0900

    #19 fix js

--- liff-prac.js
+++ liff-prac.js
@@ -10,13 +10,9 @@ function initializeApp(data) {
         output = document.getElementById("output");
         textbox = document.getElementById("input");
         output.innerText = textbox.value;
-        const json = JSON.stringify({
-            type: 'text',
-            text: "You've successfully sent a message! Hooray!" + output.textContent
-        })
         liff.sendMessages([{
             type: 'text',
-            text: "You've successfully sent a message! Hooray!" + output.textContent
+            text: output.textContent
         }]).then(function () {
             liff.closeWindow()
         }).catch(function (error) {
